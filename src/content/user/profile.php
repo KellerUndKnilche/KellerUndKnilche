6134158c887a00e95b1d635c99794502a6017f29
Stefan Berecz <108223133+StefanBerecz@users.noreply.github.com>
2025-03-30 00:17:10 +0100

    Fixed profile.php and added profile data
    fixed navigation bar and reroute logic and added profile data from session

--- src/content/user/profile.php
+++ src/content/user/profile.php
@@ -1,18 +1,39 @@
 <?php 
   $pageTitle = 'Keller & Knilche Profile';
+  require_once('../../config/dbAccess.php');
   require_once('../../includes/header.php');
   require_once('../../includes/nav.php');
-  require_once('../../config/dbAccess.php');
 
-  if(!isset($_SESSION['user']) || !isset($_COOKIE['user_id']) || $_COOKIE['user_id'] != $_SESSION['user']['id']) {
-    // Session und Cookies sind nicht gesetzt oder stimmen nicht überein
+  if (!isset($_SESSION['user'])) {
+    // Session nicht gesetzt
     header("Location: /content/user/login.php");
     exit();
   }
+
+  if (isset($_COOKIE['user_id']) && $_COOKIE['user_id'] != $_SESSION['user']['id']) {
+    // Cookie ist gesetzt aber stimmt nicht mit Session überein
+    session_destroy();
+    setcookie('user_id', '', time() - 3600, '/'); // Cookie löschen (eine Stunde in der Vergangenheit setzen)
+    setcookie('username', '', time() - 3600, '/'); // Cookie löschen
+    header("Location: /content/user/login.php");
+    exit();
+  }
+
+  $username = $_SESSION['user']['username'];
+  $email = $_SESSION['user']['email'];
+  $last_login = $_SESSION['user']['last_login'];
 ?>
 <div class="profile-container">
-  <h2>DungeonMaster_666</h2>
-  
+  <h1><?php echo $username?></h1>
+  <div class="card mb-3">
+    <p>Email: <?php echo $email?></p>
+  </div>
+  <div class="card mb-3">
+    <p>Letzter Login: <?php echo $last_login?></p>
+  </div>
+  <div class="card mb-3">
+    <p>Rang: <span class="badge bg-primary">Kellermeister</span></p>
+  </div>
   <div class="profile-stats">
     <div class="stat-item">
       <p class="stat-label">Heroes Vanquished</p>
