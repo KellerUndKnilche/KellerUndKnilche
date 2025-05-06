383248471db373e7d024110aab7dc3f7b1eb1b84
Tarik Yilmaz <tarik.yilmaz.email@gmail.com>
2025-05-06 23:41:14 +0200

    created impressum.php

--- src/content/legal/impressum.php
+++ src/content/legal/impressum.php
@@ -5,9 +5,24 @@ $pageDescription = 'Impressum – weil das Gesetz es so will, aber wir würden l
 require_once('../../includes/header.php');
 require_once('../../includes/nav.php');
 ?>
-<main class="container mt-5">
+
+<main class="impressum">
   <h1>Impressum</h1>
+  <br>
+  <h2>Keller & Knilche</h2>
+  <p>Kostenloses Idle Clicker-Spiel</p> <!-- vielleicht hast du eine bessere Beschreibung als ich :)) -->
+
+  <br>
+  <h4>Betreiber</h4>
   <p>Max Mustermann</p>
-  <p>Platzhalter für Impressum–Inhalte.</p>
+  <p>[Straße]</p>
+  <p>[PLZ, Ort]</p>
+  <p>Österreich</p>
+  <p>[E-Mail-Adresse]</p>
+  <p>[Telefonnummer]</p>
+
+  <br>
+  <p><strong>Diese Website dient ausschließlich der kostenlosen Unterhaltung und verfolgt keine kommerziellen Zwecke.</strong></p>
+
 </main>
 <?php require_once('../../includes/footer.php'); ?>
