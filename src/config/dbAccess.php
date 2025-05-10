439ba177511859d0675a78e302975c1e2b68988a
Paul <if24b042@technikum-wien.at>
2025-05-10 12:02:53 +0200

    Balancing3 + Update Preiskalk in PHP

--- src/config/dbAccess.php
+++ src/config/dbAccess.php
@@ -514,8 +514,8 @@
         if ($row = $result->fetch_assoc()) {
             $basispreis = (float)$row['basispreis'];
             $stmt->close();
-            // Preisformel: basispreis + (id^3 * level)
-            return $basispreis + (pow($upgradeId, 3) * $currentLevel);
+            // Preisformel: basispreis * 1,15^level
+            return $basispreis * pow(1.15, $currentLevel);
         } else {
             $stmt->close();
             error_log("calculateUpgradeCostPHP: Upgrade mit ID $upgradeId nicht gefunden.");
