7ec5d1065c4baebd5909c7e1448814f66de9d927
Stefan Berecz <108223133+StefanBerecz@users.noreply.github.com>
2025-05-10 15:31:42 +0200

    Rework Footer & add Github Link to Navbar

--- src/includes/footer.php
+++ src/includes/footer.php
@@ -1,41 +1,18 @@
 <footer class="container-fluid bg-dark text-light mt-5">
+<div class="d-lg-flex justify-content-center d-flex flex-wrap align-items-center">  
   <ul class="nav justify-content-center">
-    <li class="nav-item">
-      <a class="nav-link text-light" href="/">Spiel</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-light" href="/statistiken">Statistiken</a>
-    </li>
-    <?php if (isset($_SESSION['user'])) { 
-          if ($_SESSION['user']['isAdmin'] == 1) { ?>
-            <li class="nav-item">
-              <a class="nav-link text-light" href="/admin">Admin-Armaturenbrett</a>
-            </li>
-          <?php } ?>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="/profil">Profil</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link text-light" href="/logout">Logout</a>
-          </li>
-    <?php } else { ?>
-  <li class="nav-item">
-    <a class="nav-link text-light" href="/registrierung">Registrierung</a>
-  </li>
-  <li class="nav-item">
-    <a class="nav-link text-light" href="/login">Login</a>
-  </li>
-<?php } ?>
-    <li class="nav-item">
-      <a class="nav-link text-light" href="/impressum">Impressum</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-light" href="/datenschutz">Datenschutz</a>
-    </li>
-    <li class="nav-item">
-      <a class="nav-link text-light" href="/nutzungsbedingungen">Nutzungsbedingungen</a>
-    </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="/impressum">Impressum</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="/datenschutz">Datenschutz</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link text-light" href="/nutzungsbedingungen">Nutzungsbedingungen</a>
+      </li>
   </ul>
+</div>
+  
 </footer>
 <script src="/assets/js/fontSwitcher.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
