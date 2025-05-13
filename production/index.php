6047844e2bce8f5ffa46d135fa79bbc8f2de5636
Stefan Berecz <108223133+StefanBerecz@users.noreply.github.com>
2025-05-13 23:55:34 +0200

    Knopf kann nicht gezogen werden

--- production/index.php
+++ production/index.php
@@ -21,7 +21,7 @@ require_once('includes/nav.php');
             <span id="proSekunde"></span>
         </div>
         -->
-        <img id="click_button" class="gameButton" src="/assets/img/dungeon.png" alt="Keller-Knopf"/>
+        <img id="click_button" class="gameButton" src="/assets/img/dungeon.png" alt="Keller-Knopf" draggable="false" ondragstart="return false;"/>
     </section>
     
     
