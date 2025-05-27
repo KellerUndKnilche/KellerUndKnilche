80632571c917b09f3338499ff8f26e7d6b509b66
Stefan Berecz <108223133+StefanBerecz@users.noreply.github.com>
2025-05-27 18:03:41 +0200

    Eigene CSS für Landing Page

--- src/includes/header.php
+++ src/includes/header.php
@@ -20,6 +20,9 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="/assets/css/font.css">
     <link rel="stylesheet" href="/assets/css/style.css">
+    <?php if (isset($pageTitle) && $pageTitle === 'Keller & Knilche - Gewinne maximieren, Helden minimieren'): ?>
+    <link rel="stylesheet" href="/assets/css/homepage.css">
+    <?php endif; ?>
     <?php if (isset($pageTitle) && $pageTitle === 'Datenschutz'): ?>
     <link rel="stylesheet" href="/assets/css/datenschutz.css">
     <?php endif; ?>
